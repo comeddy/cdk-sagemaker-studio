c163a9767a2e60f7fc01dc4e06f3f5df974e1b3c
young <youngjik@amazon.com>
2023-07-23 23:11:50 +0900

    create sagemaker studio with cdk

--- bin/cdk-sagemaker-studio.ts
+++ bin/cdk-sagemaker-studio.ts
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
+
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CdkSagemakerStudioStack } from '../lib/cdk-sagemaker-studio-stack';
 
 const app = new cdk.App();
 new CdkSagemakerStudioStack(app, 'CdkSagemakerStudioStack', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
+
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -18,4 +22,4 @@ new CdkSagemakerStudioStack(app, 'CdkSagemakerStudioStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+// });
--- lib/cdk-sagemaker-studio-stack.ts
+++ lib/cdk-sagemaker-studio-stack.ts
@@ -1,16 +1,52 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sagemaker from 'aws-cdk-lib/aws-sagemaker';
+import { Role, ServicePrincipal, ManagedPolicy } from 'aws-cdk-lib/aws-iam';
 
 export class CdkSagemakerStudioStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const region = props?.env?.region;
+    
+    // create a SageMakerExecutionRole-${region}-cdk for the SageMaker Studio
+    let sagemakerExecutionRole = new Role(this, 'SageMakerExecutionRole', {
+      assumedBy: new ServicePrincipal('sagemaker.amazonaws.com'),
+      roleName: `SageMakerExecutionRole-${region}-cdk`,
+      description: 'SageMaker execution role'
+    });  
+    
+    sagemakerExecutionRole.addManagedPolicy(
+      ManagedPolicy.fromAwsManagedPolicyName('AmazonSageMakerFullAccess')
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkSagemakerStudioQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+    const userSettings = {
+      executionRole: sagemakerExecutionRole.roleArn,
+    }
+
+    // create a SageMaker domain
+    const defaultVpc = cdk.aws_ec2.Vpc.fromLookup(this, 'DefaultVpc', {
+      isDefault: true
+    });
+
+    const vpcSubnets = defaultVpc.selectSubnets({
+      subnetType: cdk.aws_ec2.SubnetType.PUBLIC
+    });
+
+    const domain = new sagemaker.CfnDomain(this, 'SageMakerDomain', {
+      authMode: 'IAM',
+      domainName: 'SageMakerDomain',
+      defaultUserSettings: userSettings,
+      subnetIds: vpcSubnets.subnetIds,
+      vpcId: defaultVpc.vpcId,
+    });
+
+    const profile = {'team': 'data-sciences', 'name': 'youngjin'}
+    new sagemaker.CfnUserProfile(this, 'SageMakerUserProfile', {
+      domainId: domain.attrDomainId,
+      userProfileName: `${profile.team}-${profile.name}`,
+      userSettings: userSettings,
+    });
+  }  
 }
+
